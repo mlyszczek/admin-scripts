0bf17121b1c56a5a0e2c2aa4cf86d8190fa9000c
Michał Łyszczek <michal.lyszczek@bofc.pl>
2022-04-13 23:49:00 +0200

    crypto: add support for attached header
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- crypto/create-new-client.sh
+++ crypto/create-new-client.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
 	echo "usage: $0 <name>"
+	echo "       $0 <name> <dev> <pass>"
 	echo "where"
 	echo "name:   hostname of machine for which key is generated"
+	echo "pass:   use custom pass instead of generating random key"
+	echo "dev:    create luks on specified dev, header will be attached to this dev"
 	exit 1
 fi
 
-tmp=$(mktemp)
-truncate -s1 $tmp
 mkdir $1
-dd if=/dev/urandom of=$1/keyfile bs=128 count=1
-cryptsetup luksFormat -c aes-xts-plain64 -s 256 -h sha256 -d $1/keyfile \
-		--header $1/header $tmp << EOF
-#YES${comment_confirm_creation_of_missing_header_file}
-#EOF
-rm $tmp
+
+if [ "$2" ]; then
+	fdisk -l $2
+	echo "ARE YOU SURE YOU WANNA DESTROY THAT DISK/PARTITION?! ARE YOU?"
+	echo "TYPE CAPITAL 'YES' IF YOU ARE"
+	read choice
+	if [ x$choice != xYES ]; then
+		echo "you're not sure, that's ok, better safe than sorry"
+		echo "double check and retry"
+		exit 0
+	fi
+	printf "%s" "$3" > $1/keyfile
+	cryptsetup luksFormat -c aes-xts-plain64 -s 256 -h sha256 -d $1/keyfile $2
+else
+	tmp=$(mktemp)
+	truncate -s1 $tmp
+	dd if=/dev/urandom of=$1/keyfile bs=128 count=1
+	cryptsetup luksFormat -c aes-xts-plain64 -s 256 -h sha256 -d $1/keyfile \
+			--header $1/header $tmp << EOF
+YES${comment_confirm_creation_of_missing_header_file}
+EOF
+	rm $tmp
+fi
+
 cat << EOF > $1/disks
 # /path/to/disk   mapper-friendly-name
 # path to disk can be path to /dev/disk/by-id/* or similar
---
+++ crypto/open-disk-locally.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+	echo "usage: $0 <hostname> <dev>"
+	exit 1
+fi
+
+keyfile=$1/keyfile
+header=$2/header
+
+if [ -s $header ]; then
+	cryptsetup luksOpen --key-file $keyfile --header $header $2 local-$1
+else
+	cryptsetup luksOpen --key-file $keyfile $2 local-$1
+fi
