beaf4025a5ffc5428b0b270c79a952515690a0cc
Michał Łyszczek <michal.lyszczek@bofc.pl>
2023-08-01 11:36:17 +0200

    compile-kernel: do not use binpkg for module rebuild
    and don't crash when boot is already mounted
    
    Signed-off-by: Michał Łyszczek <michal.lyszczek@bofc.pl>

--- compile-kernel-and-do-initrd.sh
+++ compile-kernel-and-do-initrd.sh
@@ -27,7 +27,7 @@ echo ":: prepare kernel for external modules"
 make modules_prepare
 
 echo ":: rebuild external modules"
-emerge -j @module-rebuild
+emerge -j @module-rebuild --usepkg-exclude '*/*'
 
 echo ":: create initrd"
 if [ "$1" ]; then
@@ -37,7 +37,9 @@ else
 fi
 
 echo ":: generating new grub config"
+set +e
 mount /boot >/dev/null 2>/dev/null # /boot might be already mounted
+set -e
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo ":: all done:)"
